bda7108547515f7403291cec3ad9c324c7b1576d
Taylor Fausak <taylor@fausak.me>
2015-10-19 20:33:49 -0500

    Clearly separate sections

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// API v1
+
 const apiV1 = express.Router();
+
 apiV1.get('/', (request, response) => {
   response.status(501).json(null);
 });
 
+// API
+
 const api = express.Router();
 api.use('/v1', apiV1);
+
+// Application
 
 const app = express();
 app.use(morgan('combined'));
 app.use('/api', api);
+
+// Server
 
 const port = process.env.PORT || 8888;
 const server = app.listen(port, () => {
