5348683556b2731ff042e0948dec7ce62ead8f49
Taylor Fausak <taylor@fausak.me>
2015-10-19 20:19:47 -0500

    Add some logging

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('combined'));
 app.get('/', (request, response) => {
   response.status(501).json(null);
 });
