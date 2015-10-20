f6a121dcafa39920b2535713ed020d6ba56e280e
Taylor Fausak <taylor@fausak.me>
2015-10-19 20:21:20 -0500

    Move the API to /api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
-app.use(morgan('combined'));
-app.get('/', (request, response) => {
+const api = express.Router();
+api.get('/', (request, response) => {
   response.status(501).json(null);
 });
+
+const app = express();
+app.use(morgan('combined'));
+app.use('/api', api);
 
 const port = process.env.PORT || 8888;
 const server = app.listen(port, () => {
